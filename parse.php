978a7080ce8ac48a4b0ae8d75c687ecba1967d7b
koxaa <kruhmalik7@gmail.com>
2021-02-22 23:49:49 +0100

    not bad2

--- parse.php
+++ parse.php
@@ -1,3 +1,36 @@
 <?php
+ini_set('display_errors', 'stderr'); // Pro výpis varování na standardní chybový výstup
+
+/******* Hlavní scénář *******/
+check_options();
+
+
+
+
+/********* Funkce *********/
+
+function check_options() {
+
+    global $argc;
+    $options = getopt("",["help","stats::"]);
+
+    if ($argc == 1) {
+        return 0;
+    } elseif (array_key_exists("help" ,$options)) {
+
+        if ($argc != 2) {
+            exit(10);
+        } else {
+            echo "Skript typu filtr (parse.php v jazyce PHP 7.4)\n";
+            echo "načte ze standardního vstupu zdrojový kód v IPP-code21,\n";
+            echo "zkontroluje lexikální a syntaktickou správnost kódu\n";
+            echo "a vypíše na standardní výstup XML reprezentaci programu.\n";
+        }
+    
+    } elseif (array_key_exists("stats", $options)) {
+        // TODO
+        echo $options["stats"],"\n";
+    }
+}
 
 ?>
---
+++ readme1.md
@@ -0,0 +1,1 @@
+# Implementační dokumentace k 1. úloze do IPP 2020/2021<br/>Jméno a příjmení: Kostiantyn Krukhmalov<br/>Login: xkrukh00
